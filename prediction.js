8599e494ba9ad4174ed0b6b5419170d93dad2a83
Honey-Whisper <honeywhisper69@gmail.com>
2025-12-26 12:15:29 +0100

    Add files via upload

---
+++ prediction.js
@@ -0,0 +1,9 @@
+function initPrediction() {
+    document.getElementById('predictBtn')?.addEventListener('click', () => {
+        alert("Prediction live hoga jab full prediction ABI add kar denge!\nAbhi prize locked hai – 3,875 $ROX 🔥");
+    });
+
+    document.getElementById('shareCardBtn')?.addEventListener('click', () => {
+        alert("Card banega prediction success ke baad!");
+    });
+}
