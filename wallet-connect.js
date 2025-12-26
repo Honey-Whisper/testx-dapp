a4b0b92989c4b8edfd69d16d367959ff1637d2d7
Honey-Whisper <honeywhisper69@gmail.com>
2025-12-26 12:17:24 +0100

    Add files via upload

---
+++ wallet-connect.js
@@ -0,0 +1,24 @@
+let provider, signer, user;
+
+async function connectWallet() {
+    if (!window.ethereum) {
+        alert("MetaMask install karo bhai!");
+        return;
+    }
+
+    try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner();
+        user = (await signer.getAddress()).toLowerCase();
+
+        initContracts();
+        updateUIAfterConnect();
+        loadAllData();
+    } catch (err) {
+        alert("Connect nahi hua: " + err.message);
+    }
+}
+
+document.getElementById('connectBtn').onclick = connectWallet;
+document.getElementById('disconnectBtn').onclick = () => location.reload();
