422c9451b307aef88b142e82fb148afe96d426c1
Honey-Whisper <honeywhisper69@gmail.com>
2026-01-21 10:22:19 +0100

    Delete prediction.js

--- prediction.js
+++
@@ -1,9 +0,0 @@
-function initPrediction() {
-    document.getElementById('predictBtn')?.addEventListener('click', () => {
-        alert("Prediction live hoga jab full prediction ABI add kar denge!\nAbhi prize locked hai – 3,875 $ROX 🔥");
-    });
-
-    document.getElementById('shareCardBtn')?.addEventListener('click', () => {
-        alert("Card banega prediction success ke baad!");
-    });
-}
