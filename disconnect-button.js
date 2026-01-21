499a44e7a000ff8a7c75db59aae71f1500b5ca50
Honey-Whisper <honeywhisper69@gmail.com>
2026-01-21 10:18:06 +0100

    Delete disconnect-button.js

--- disconnect-button.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('disconnectBtn')?.addEventListener('click', () => {
-    provider = signer = contract = user = null;
-    document.getElementById('connectBtn').style.display = 'inline-block';
-    document.getElementById('disconnectBtn').style.display = 'none';
-    document.getElementById('walletAddress').style.display = 'none';
-    document.querySelectorAll('.main-btn').forEach(b => b.disabled = true);
-    alert("Wallet disconnected!");
-});
